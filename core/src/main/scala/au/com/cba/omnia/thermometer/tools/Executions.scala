e1098dfcafb442f40c778cdae7c526f5f17f8b70
Todd Owen <todd.owen@cba.com.au>
2016-12-21 12:16:01 +1100

    Rejected execution handler (#68)

--- core/src/main/scala/au/com/cba/omnia/thermometer/tools/Executions.scala
+++ core/src/main/scala/au/com/cba/omnia/thermometer/tools/Executions.scala
@@ -19,7 +19,7 @@ import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration.Duration.Inf
 import scala.util.Try
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{Executors, RejectedExecutionHandler, ThreadPoolExecutor}
 
 import com.twitter.scalding.{Config, Execution, Mode}
 
@@ -27,9 +27,29 @@ object Executions {
   /** Run the specified execution context, returns an optional error state (None indicates success). */
   def runExecution[T](config: Config, mode: Mode, execution: Execution[T]): Try[T] = {
     val es     = Executors.newCachedThreadPool()
+    // Occasionally (when the system is under load), RejectedExecutionException has been observed
+    // due to an execution request submitted to the ThreadPoolExecutor after shutdown has begun.
+    // The consequences of this are unclear; often the test still succeeds, on the other hand
+    // it has been the last output received from some (but not all) of the coppersmith Travis builds
+    // which have intermittently stalled (see CommBank/RedMetalSquad#135).
+    // In a continuing attempt to diagnose this problem, replace the executor's default
+    // handler (AbortPolicy) with a custom handler that ensures all requests are executed.
+    es.asInstanceOf[ThreadPoolExecutor].setRejectedExecutionHandler(RejectedExecutionHandler)
     val cec    = ExecutionContext.fromExecutorService(es)
     val result = Try(Await.result(execution.run(config, mode)(cec), Inf))
     es.shutdown()
     result
+  }
+
+  // Handler that runs the task in the caller's thread, with verbose logging to stderr.
+  // (Choosing to bypass the logging framework, as this is unexpected behaviour and should never be silenced).
+  object RejectedExecutionHandler extends RejectedExecutionHandler {
+    def rejectedExecution(r: Runnable, es: ThreadPoolExecutor) {
+      System.err.println(s"Execution request rejected by ${es}")
+      val t = Thread.currentThread
+      System.err.println(s"Executing ${r} in caller thread instead (name=${t.getName}, id=${t.getId})")
+      r.run
+      System.err.println(s"Execution of ${r} complete")
+    }
   }
 }
